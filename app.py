bd344090934ff71a7ca319f7a065d5b15e04877e
OBadDogO <141470009+OBadDogO@users.noreply.github.com>
2023-08-12 10:18:54 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,21 @@
 from flask import Flask, request, abort
 
-#from events.basic import *
-#from line_bot_api import *
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent, StickerSendMessage, ImageSendMessage, LocationSendMessage
-)
-
-line_bot_api = LineBotApi('kQavKvxNS5PDFgBV/8mgLPpQZmmdToFeaneQMCvj7wZvP5xe67lxDFvnpCkDwFaAT9xGyOgR2C+HeKWjDcIPcEsiqtMAL809pC5MnwVejhqZR30540WJZlCBBvn7ce/tqMJy93FUic3JY8gFdN3PMAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('248fb9d60f05e53f3fd713f2e184f598')
+from events.basic import *
+from line_bot_api import *
 
 app = Flask(__name__)
 
-@app.route("/callback" , methods=['POST'])
+
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers["X-Line-Signature"]
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
+    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " +body)
+    app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -31,3 +23,40 @@ def callback():
         abort(400)
 
     return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+
+    message_text = str(event.message.text).lower()
+
+    if message_text == '@關於我們':
+        about_us_event(event)
+
+    elif message_text == '@營業據點':
+        location_event(event)
+
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    welcome_msg = """Hello! 您好，歡迎您成為 Master SPA 的好友！
+
+我是Master SPA的小幫手 
+
+-想預約按摩/臉部淨化護理服務都可以直接跟我互動喔~
+-直接點選下方【歡迎光臨專屬您的SPA】選單功能
+
+-期待您的光臨！"""
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=welcome_msg))
+
+
+@handler.add(UnfollowEvent)
+def handle_unfollow(event):
+    print(event)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ line_bot_api.py
@@ -0,0 +1,13 @@
+# pip install line-bot-sdk
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent, StickerSendMessage, ImageSendMessage, LocationSendMessage
+)
+
+line_bot_api = LineBotApi('kQavKvxNS5PDFgBV/8mgLPpQZmmdToFeaneQMCvj7wZvP5xe67lxDFvnpCkDwFaAT9xGyOgR2C+HeKWjDcIPcEsiqtMAL809pC5MnwVejhqZR30540WJZlCBBvn7ce/tqMJy93FUic3JY8gFdN3PMAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('248fb9d60f05e53f3fd713f2e184f598')
