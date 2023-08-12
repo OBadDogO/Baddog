639c8e583b4b27ab7bf8004f1a5949580be6219c
OBadDogO <141470009+OBadDogO@users.noreply.github.com>
2023-08-12 13:42:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -40,6 +40,12 @@ def handle_message(event):
     elif message_text == '@預約服務':
         service_category_event(event)
 
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    data = dict(parse_qsl(event.postback.data))
+    if data.get('action') == 'service':
+        service_event(event)
+
 @handler.add(FollowEvent)
 def handle_follow(event):
     welcome_msg = """Hello! 您好，歡迎您成為 Master SPA 的好友！
